23af1a0700a093f0d7f38747139b76157a5c0041
Arnab Mandal <30294886+Arnab224@users.noreply.github.com>
2025-05-28 12:37:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users", userRoutes);
 app.use(cors({
-  origin: "to-do-app-rosy-nu.vercel.app",
+  origin: "https://to-do-app-rosy-nu.vercel.app/",
   credentials: true
 }));
 
